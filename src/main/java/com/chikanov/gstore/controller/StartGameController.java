46a5704f8dd2243cc85f1ef38aa56bce3a4fdc95
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-05-26 22:30:20 +0300

    update properties

--- src/main/java/com/chikanov/gstore/controller/StartGameController.java
+++ src/main/java/com/chikanov/gstore/controller/StartGameController.java
@@ -1,5 +1,6 @@
 package com.chikanov.gstore.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 public class StartGameController {
     @PostMapping("/miniapp_controller")
-    public ResponseEntity<?> startGame(@RequestBody String postBody)
+    public ResponseEntity<?> startGame(@RequestBody String postBody,HttpServletRequest request)
     {
+        System.out.println(request.getQueryString());
+        System.out.println(request.getRequestURI());
+        System.out.println(request.getHeaderNames());
         System.out.println(postBody);
         return ResponseEntity.ok(postBody);
     }
