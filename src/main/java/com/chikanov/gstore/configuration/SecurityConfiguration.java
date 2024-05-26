ca8ec62fdaba5fa4eddcd6d787e2c24bc041e9b5
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-05-26 22:02:54 +0300

    update properties

--- src/main/java/com/chikanov/gstore/configuration/SecurityConfiguration.java
+++ src/main/java/com/chikanov/gstore/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.chikanov.gstore.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,7 +14,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain getSecure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth->
-                auth.anyRequest().permitAll());
+                auth.anyRequest().permitAll()).csrf(csrf->csrf.disable());
         return http.build();
     }
 }
