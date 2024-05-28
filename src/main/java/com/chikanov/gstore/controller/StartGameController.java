0dea5819e103b8cc20156e93c86d1ddad562d0e9
chikanov <chikanovivan@gmail.com>
2024-05-28 15:59:45 +0300

    add send message

--- src/main/java/com/chikanov/gstore/controller/StartGameController.java
+++ src/main/java/com/chikanov/gstore/controller/StartGameController.java
@@ -1,19 +1,26 @@
 package com.chikanov.gstore.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 
 @Controller
 public class StartGameController {
+    @Value("${token.value}")
+    private String token;
+
     @PostMapping("/miniapp_controller")
     public ResponseEntity<?> startGame(@RequestBody String postBody)
     {
@@ -22,9 +29,17 @@ public class StartGameController {
         return ResponseEntity.ok(postBody);
     }
     @PostMapping("/bot_controller")
-    public ResponseEntity<?> sendBot(@RequestBody String postBody)
-    {
+    public ResponseEntity<?> sendBot(@RequestBody String postBody) throws IOException {
         System.out.println(postBody);
+        String url = String.format("https://api.telegram.org/bot%s/sendMessage", token);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        ObjectMapper om = new ObjectMapper();
+        JsonNode json = om.readTree(postBody.getBytes(StandardCharsets.UTF_8));
+        String body = "{ \"chat_id\":" + json.get("chat").get("id") + ", \"text\": \"test\"}";
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        RestTemplate rest = new RestTemplate();
+        rest.exchange(url, HttpMethod.POST, request, String.class);
         return ResponseEntity.ok(postBody);
     }
     @GetMapping("/miniapp_controller")
