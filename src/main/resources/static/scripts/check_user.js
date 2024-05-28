3d01c0bef697fbb389a65c32532f605c57079f41
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-05-28 22:30:24 +0300

    refactor

--- src/main/resources/static/scripts/check_user.js
+++ src/main/resources/static/scripts/check_user.js
@@ -1,3 +1,5 @@
+window.addEventListener("load", ()=>{
 let header = document.createElement("h3");
 header.innerText = `Hello ${window.Telegram.WebApp.initDataUnsafe.user.username}!`;
 document.body.addChild(header);
+});
