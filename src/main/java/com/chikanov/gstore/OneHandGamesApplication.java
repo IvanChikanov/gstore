66bafc079478d823d241fbcb560aa90a9dce5cba
chikanov <chikanovivan@gmail.com>
2024-05-29 17:34:54 +0300

    add user checked

--- src/main/java/com/chikanov/gstore/OneHandGamesApplication.java
+++ src/main/java/com/chikanov/gstore/OneHandGamesApplication.java
@@ -2,7 +2,6 @@ package com.chikanov.gstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ResourceLoader;
 
 @SpringBootApplication
 public class OneHandGamesApplication {
--- src/main/java/com/chikanov/gstore/controller/StartGameController.java
+++ src/main/java/com/chikanov/gstore/controller/StartGameController.java
@@ -1,31 +1,22 @@
 package com.chikanov.gstore.controller;
 
 import com.chikanov.gstore.services.ReadJsonService;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.client.RestTemplate;
-
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
-@Slf4j
 @Controller
 public class StartGameController {
-    @Value("${token.value}")
-    private String token;
 
     @Autowired
-    ReadJsonService readJsonService;
+    private ReadJsonService readJsonService;
 
     @PostMapping("/miniapp_controller")
     public ResponseEntity<?> startGame(@RequestBody String postBody)
