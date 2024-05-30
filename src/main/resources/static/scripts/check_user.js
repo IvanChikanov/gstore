4d8ebb903f91f8bf7bcb178535123e094d9c4c9a
chikanov <chikanovivan@gmail.com>
2024-05-30 16:38:44 +0300

    add user checked

--- src/main/resources/static/scripts/check_user.js
+++ src/main/resources/static/scripts/check_user.js
@@ -14,7 +14,7 @@ async function sendUserData()
 {
     let req = await fetch("/check", {
     method: "POST",
-    body: `{"auth": "${encodeURI(window.Telegram.WebApp.initData)}", "data": "${window.Telegram.WebApp.initDataUnsafe}" }`
+    body: `{"auth": "${decodeURI(decodeURI(window.Telegram.WebApp.initData))}", "data": "${window.Telegram.WebApp.initDataUnsafe}" }`
     });
     return await req.status;
 }
