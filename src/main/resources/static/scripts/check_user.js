7a828d42e0293ec4b006935d52d7566e44723d88
chikanov <chikanovivan@gmail.com>
2024-05-30 17:07:27 +0300

    add user checked

--- src/main/resources/static/scripts/check_user.js
+++ src/main/resources/static/scripts/check_user.js
@@ -8,14 +8,14 @@ else{
     let header = document.createElement("H3");
     header.innerText = `Hello ${window.Telegram.WebApp.initDataUnsafe.query_id}! requestStatus: ${check} ${window.Telegram.WebApp.initDataUnsafe.toString()}`;
     document.body.appendChild(header);
-    window.Telegram.WebApp.sendData();
+    console.log(window.Telegram.WebApp.initDataUnsafe);
 }
 });
 async function sendUserData()
 {
     let req = await fetch("/check", {
     method: "POST",
-    body: `{"auth": "${decodeURI(decodeURI(window.Telegram.WebApp.initData))}", "data": "${window.Telegram.WebApp.initDataUnsafe}" }`
+    body: `{"auth": "${window.Telegram.WebApp.initData}", "data": "${window.Telegram.WebApp.initDataUnsafe}" }`
     });
     return await req.status;
 }
