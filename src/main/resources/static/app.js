66a040746f4ed5464a216aa448fdc0587b36dd11
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-06-04 22:39:58 +0300

    get main window added

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,22 +1,22 @@
 class Application
 {
-    tg;
+    static tg;
+    window;
     constructor()
     {
         this.tg = window.Telegram.WebApp;
         this.tg.ready();
         this.tg.expand();
         this.startApp();
-    
     }
     async startApp()
     {
         let childos = document.createElement("h1");
         try
         {
+            this.window = new MainWindow();
             let response = await Sender.request("hello", "main");
             childos.innerText = "Hello, " + this.tg.initDataUnsafe.user.username + "!" + await response.text();
-            
         }
         catch(ex)
         {
---
+++ src/main/resources/static/lib/main_window.js
@@ -0,0 +1,33 @@
+class MainWindow{
+    #body;
+    #mainGrid;
+    #header;
+    #settingButton;
+    constructor()
+    {
+        this.#body = document.body;
+        this.#createMainGrid();
+        this.#createHeader();
+        this.#settingButton();
+    }
+    #createMainGrid()
+    {
+        this.#mainGrid = document.createElement("DIV");
+        this.#mainGrid.classList.add("main");
+        this.#body.appendChild(div);
+    }
+    #createHeader()
+    {
+        this.#header = document.createElement("DIV");
+        this.#header.classList.add("header");
+        this.#header.innerText = `Привет, ${window.Telegram.WebApp.user.username}!`;
+        this.#mainGrid.appendChild(this.#header);
+    }
+    #settingButton()
+    {
+        this.#settingButton = document.createElement("DIV");
+        this.#settingButton.classList.add("game_button");
+        this.#settingButton.innerText = "*";
+        this.#mainGrid.appendChild(this.#settingButton);
+    }
+}
