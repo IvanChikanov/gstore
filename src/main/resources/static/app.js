65cff49315cc0f4f8c2bf59678bc0b6f888e0ceb
chikanov <chikanovivan@gmail.com>
2024-06-04 18:53:36 +0300

    add user checked

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,13 +3,13 @@ class Application
     tg;
     constructor()
     {
-        let response = Sender.request("hello", "main");
         this.tg = window.Telegram.WebApp;
         this.tg.ready();
         this.tg.expand();
         let childos = document.createElement("h1");
         childos.innerText = "Hello, " this.tg.initDataUnsafe().user.username; + "!";
         document.body.appendChild(childos);
+        let response = Sender.request("hello", "main");
     }
 }
 let app = new Application();
