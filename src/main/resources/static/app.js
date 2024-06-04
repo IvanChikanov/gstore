ea141e80df251381deb9c5eb3477844170dddc2a
chikanov <chikanovivan@gmail.com>
2024-06-04 18:41:15 +0300

    add user checked

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,8 +1,11 @@
 class Application
 {
+    tg;
     constructor()
     {
         let response = Sender.request("hello", "main");
+        this.tg = = window.Telegram.WebApp;
+        this.tg.expand();
     }
 }
 let app = new Application();
