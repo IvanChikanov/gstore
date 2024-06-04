fd8aa624db62dfb1f6e12a3fe6d56bf4a0c50cad
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-06-04 21:12:49 +0300

    refactoring message reader

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -7,7 +7,7 @@ class Application
         this.tg.ready();
         this.tg.expand();
         let childos = document.createElement("h1");
-        childos.innerText = "Hello, " this.tg.initDataUnsafe().user.username; + "!";
+        childos.innerText = "Hello, " + this.tg.initDataUnsafe().user.username + "!";
         document.body.appendChild(childos);
         let response = Sender.request("hello", "main");
     }
