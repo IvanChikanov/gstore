364bff40025653d9b6bc885f0d6ea5dde19ea3d2
chikanov <chikanovivan@gmail.com>
2024-06-13 18:33:25 +0300

    save users and chats

--- src/main/resources/static/lib/sender.js
+++ src/main/resources/static/lib/sender.js
@@ -9,7 +9,7 @@ class Sender
                 headers: 
                 {
                     "Authorization": window.Telegram.WebApp.initData,
-                    "User-Chat": `${window.Telegram.WebApp.initDataUnsafe.chat.id}~~${window.Telegram.WebApp.initDataUnsafe.user.id}`,
+                    "User-Chat": `${window.Telegram.WebApp.initDataUnsafe.chat.id}`,
                     "Content-Type": "application/json"
                 },
                 body: body
