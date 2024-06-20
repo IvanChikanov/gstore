dd1627cb6298396ec093420ab3dfdb044cbe361e
chikanov <chikanovivan@gmail.com>
2024-06-20 15:06:50 +0300

    front

--- src/main/resources/static/type_script/App.ts
+++ src/main/resources/static/type_script/App.ts
@@ -1,12 +1,15 @@
+import { TelegramWebApps } from "./declare/tg";
 import Grid from "./types/Grid"
 
 class Application{
     private eventListener: any;
     private mainGrid: Grid;
+    //private webApp: TelegramWebApps.WebApp;
     constructor()
     {
         this.mainGrid = new Grid();
         document.body.appendChild(this.mainGrid.getHtml());
+        console.log(Telegram.WebApp.initDataUnsafe.user?.usernames);
     }
 }
 var app = new Application();
