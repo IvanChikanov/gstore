97e024c2726d289e7f632d7fed4188952ddac42a
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-06-21 23:03:26 +0300

    Create game

--- src/main/resources/static/type_script/types/Sizer.ts
+++ src/main/resources/static/type_script/types/Sizer.ts
@@ -5,6 +5,7 @@ export class Sizer{
     private static rect: DOMRect
     public static init(): void{
         this.rect = document.documentElement.getBoundingClientRect();
+        console.log(this.rect);
         this.height = this.rect.height;
         this.width = this.rect.width;
         window.addEventListener("resize", this.init);
