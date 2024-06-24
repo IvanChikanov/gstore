187c499b7a158433c85ae5542825b75090520a2c
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-06-24 22:42:02 +0300

    Create game

--- src/main/resources/static/type_script/types/GameScreen.ts
+++ src/main/resources/static/type_script/types/GameScreen.ts
@@ -15,7 +15,7 @@ export class GLScreen extends Ui{
         precision highp float;
         in vec2 position;
         void main() {
-          gl_Position = vec4(positi on, 1.0, 1.0);
+          gl_Position = vec4(position, 1.0, 1.0);
         }`;
     constructor(w: number, h: number)
     {
