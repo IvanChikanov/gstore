870e93c4817cf4da3f9276ebfa7de7d95654a260
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-07-11 21:18:25 +0300

    ws adventure

--- src/main/java/com/chikanov/gstore/configuration/WebSocketConfig.java
+++ src/main/java/com/chikanov/gstore/configuration/WebSocketConfig.java
@@ -3,10 +3,17 @@ package com.chikanov.gstore.configuration;
 import com.chikanov.gstore.websock.WSHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.HandshakeFailureException;
+import org.springframework.web.socket.server.HandshakeHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.util.Map;
 
 
 @Configuration
@@ -18,6 +25,21 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-            registry.addHandler(wsHandler, "/connection").setAllowedOrigins("*").setHandshakeHandler(new DefaultHandshakeHandler());
+            registry.addHandler(wsHandler, "/connection").setAllowedOrigins("*").setHandshakeHandler(new Shaker());
+    }
+    public class Shaker implements HandshakeHandler{
+
+        @Override
+        public boolean doHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws HandshakeFailureException {
+            System.out.println(request.getHeaders());
+            System.out.println(response.getHeaders());
+            System.out.println(attributes.size());
+            attributes.forEach((s, o) -> {
+                System.out.println(s);
+                System.out.println(o);
+            });
+
+            return true;
+        }
     }
 }
