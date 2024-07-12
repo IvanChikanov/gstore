9b354db07cf646be36bdbae9de42d3a9abcda323
chikanov <chikanovivan@gmail.com>
2024-07-12 17:24:16 +0300

    set inlines

--- src/main/java/com/chikanov/gstore/entity/tgentities/InlineQuery.java
+++ src/main/java/com/chikanov/gstore/entity/tgentities/InlineQuery.java
@@ -1,9 +1,13 @@
 package com.chikanov.gstore.entity.tgentities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class InlineQuery {
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("chat_type")
     private String chatType;
 }
--- src/main/java/com/chikanov/gstore/entity/tgentities/TgQueries.java
+++ src/main/java/com/chikanov/gstore/entity/tgentities/TgQueries.java
@@ -1,11 +1,14 @@
 package com.chikanov.gstore.entity.tgentities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TgQueries {
 
+    @JsonProperty("update_id")
     private Long update;
 
+    @JsonProperty("inline_query")
     private InlineQuery inlineQuery;
 }
