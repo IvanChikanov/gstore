ce3d77f9e29faa9333b4339ce54d3a62387ea498
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-08-05 21:27:42 +0300

    save chat role entities

--- src/main/java/com/chikanov/gstore/controller/MenuController.java
+++ src/main/java/com/chikanov/gstore/controller/MenuController.java
@@ -8,7 +8,10 @@ import com.chikanov.gstore.entity.tgentities.TgUser;
 import com.chikanov.gstore.enums.Role;
 import com.chikanov.gstore.services.ChatRoleService;
 import com.chikanov.gstore.services.GameTypeService;
+import com.chikanov.gstore.services.SendMessageService;
 import com.chikanov.gstore.services.UserService;
+import com.chikanov.gstore.services.tgservice.MessageService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -32,6 +35,12 @@ public class MenuController extends AbstractController{
     @Autowired
     private ChatRoleService chatRoleService;
 
+    @Autowired
+    private SendMessageService sendMessageService;
+
+    @Autowired
+    private MessageService messageService;
+
     @GetMapping("/private")
     public ResponseEntity<GameTypeFilteredDTO> getGames() throws Exception {
         return ResponseEntity.ok(gameTypeService.getActiveGames());
@@ -45,9 +54,18 @@ public class MenuController extends AbstractController{
     }
 
     @PostMapping("/send_game")
-    public ResponseEntity<?> sendGame(@RequestBody SendGameDTO sendGameDTO) throws Exception
+    public ResponseEntity<?> sendGame(@RequestAttribute("user") TgUser user, @RequestBody SendGameDTO sendGameDTO) throws Exception
     {
         System.out.println(sendGameDTO.game_id());
+        ObjectMapper om = new ObjectMapper();
+        sendMessageService.send(om.writeValueAsString(messageService.oneButtonMessage(
+                String.format("@%s приглашает вас в игру", user.getUsername()),
+                sendGameDTO.chat_id(),
+                messageService.oneButtonKeyboard(
+                        "Играть",
+                        String.format("https://t.me/Cooperation_chat_minigames_bot/coop_g_store?startapp=%s", UUID.randomUUID())))
+                )
+        );
         return ResponseEntity.ok(UUID.randomUUID());
     }
 
