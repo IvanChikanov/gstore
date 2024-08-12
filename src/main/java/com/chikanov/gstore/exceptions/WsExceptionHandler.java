f5daaac182d5056516ba8c13a951c25a94028849
chikanov <levtrozky@outlook.com>
2024-08-12 18:53:36 +0300

    ws exception

--- src/main/java/com/chikanov/gstore/exceptions/WsExceptionHandler.java
+++ src/main/java/com/chikanov/gstore/exceptions/WsExceptionHandler.java
@@ -1,16 +1,23 @@
 package com.chikanov.gstore.exceptions;
 
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
 
 public class WsExceptionHandler extends Exception{
     public WsExceptionHandler(WebSocketSession session, CloseStatus status){
         super("Произошла ошибка в WebSocket");
 
     }
-    private void switchError(CloseStatus closeStatus){
-        switch (closeStatus.getCode()){
-            default -> {}
+    public WsExceptionHandler(WebSocketSession session, String message){
+        super(message);
+        try(WebSocketSession ws = session){
+            ws.close(CloseStatus.NO_STATUS_CODE);
+        }
+        catch (IOException ex){
+
         }
     }
 }
