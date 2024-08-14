294dd72ba6fc3eee0a7e94f0e3ddebe326713442
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-08-14 22:53:10 +0300

    save game first try

--- src/main/java/com/chikanov/gstore/games/AbstractRoom.java
+++ src/main/java/com/chikanov/gstore/games/AbstractRoom.java
@@ -3,6 +3,7 @@ package com.chikanov.gstore.games;
 import com.chikanov.gstore.entity.ChatRole;
 import com.chikanov.gstore.records.ActionMessage;
 import com.chikanov.gstore.records.CloseMessage;
+import com.chikanov.gstore.records.ResultData;
 import com.chikanov.gstore.records.WsPlayer;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -10,6 +11,7 @@ import org.springframework.web.socket.WebSocketSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 public abstract class AbstractRoom<T> implements IRoom{
@@ -28,6 +30,9 @@ public abstract class AbstractRoom<T> implements IRoom{
 
     @Override
     public abstract boolean readMessage(ActionMessage message) throws Exception;
+
+    @Override
+    public abstract Set<ResultData> endGame();
 
     @Override
     public void sendMessage(WebSocketSession user, WebSocketMessage<?> message) {
