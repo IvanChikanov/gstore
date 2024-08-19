3290c6f6095301eb1bf98e7430bc77a6cc82cd0c
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-08-19 22:37:28 +0300

    refactor ws message protocol

--- src/main/java/com/chikanov/gstore/games/components/XoGameRoom.java
+++ src/main/java/com/chikanov/gstore/games/components/XoGameRoom.java
@@ -141,6 +141,14 @@ public class XoGameRoom extends AbstractRoom<XoGameRoom.XoPlayer> {
             player.getRealTimeData().setResult(result);
             players.put(session.getId(), player);
             session.sendMessage(new TextMessage(wsMessageConverter.createFullMessage(TypesOfMessage.AUTH, 0, session.getId())));
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println(players.size());
+            System.out.println();
+            System.out.println();
+            System.out.println();
             if (players.size() == max)
                 startGame();
             return true;
