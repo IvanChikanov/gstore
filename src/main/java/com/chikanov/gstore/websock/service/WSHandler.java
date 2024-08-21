11dfe3a282e5f00bc9823da0469eff8fe94ad461
IvanChikanov <131649823+IvanChikanov@users.noreply.github.com>
2024-08-21 21:43:59 +0300

    add exceptions

--- src/main/java/com/chikanov/gstore/websock/service/WSHandler.java
+++ src/main/java/com/chikanov/gstore/websock/service/WSHandler.java
@@ -63,6 +63,7 @@ public class WSHandler implements WebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        System.out.println("Ля лЯ лЯ лЯ я закрытие сессии!!!!");
         webSocketRouteService.disconnectHandler(closeStatus);
 
     }
